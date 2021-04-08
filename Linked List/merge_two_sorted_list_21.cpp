47cead3cae0fb9d9aeac17ae9e9f3c80cf55f482
Avishek kumar <46109905+avishekkumar0204@users.noreply.github.com>
2021-04-08 19:42:34 +0530

    recursive method

--- Linked List/merge_two_sorted_list_21.cpp
+++ Linked List/merge_two_sorted_list_21.cpp
@@ -4,7 +4,7 @@
  *     int val;
  *     ListNode *next;
  *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr)
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
