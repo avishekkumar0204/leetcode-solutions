66071ed3e01387217a4bed384ceaef1c3a8a833a
Avishek kumar <46109905+avishekkumar0204@users.noreply.github.com>
2021-04-09 15:43:59 +0530

    Add files via upload

--- Linked List/sort_list_148.cpp
+++ Linked List/sort_list_148.cpp
@@ -1,37 +1,58 @@
-ListNode *merge(ListNode *l1, ListNode *l2)
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution
 {
-    if (l1 == NULL)
-        return l2;
-    if (l2 == NULL)
-        return l1;
-    ListNode *ans;
-    if (l1->val < l2->val)
+public:
+    ListNode *merge(ListNode *l1, ListNode *l2)
     {
-        l1->next = merge(l1->next, l2);
-        ans = l1;
+        if (l1 == NULL)
+            return l2;
+        if (l2 == NULL)
+            return l1;
+        ListNode *ans;
+        if (l1->val < l2->val)
+        {
+            l1->next = merge(l1->next, l2);
+            ans = l1;
+        }
+        else
+        {
+            l2->next = merge(l1, l2->next);
+            ans = l2;
+        }
+        return ans;
     }
-    else
+    ListNode *mergeSort(ListNode *head)
     {
-        l2->next = merge(l1, l2->next);
-        ans = l2;
-    }
-    return ans;
-}
-ListNode *mergeSort(ListNode *head)
-{
-    if (head->next == NULL)
+        if (head->next == NULL)
+            return head;
+        ListNode *p = head, *prev = NULL, *s = head, *f = head;
+        while (f != NULL && f->next != NULL)
+        {
+            prev = s;
+            s = s->next;
+            f = f->next->next;
+        }
+        if (prev)
+            prev->next = NULL;
+        p = mergeSort(p);
+        s = mergeSort(s);
+        head = merge(p, s);
         return head;
-    ListNode *p = head, *prev = NULL, *s = head, *f = head;
-    while (f != NULL && f->next != NULL)
-    {
-        prev = s;
-        s = s->next;
-        f = f->next->next;
     }
-    if (prev)
-        prev->next = NULL;
-    p = mergeSort(p);
-    s = mergeSort(s);
-    head = merge(p, s);
-    return head;
-}
+    ListNode *sortList(ListNode *head)
+    {
+        if (head == NULL)
+            return head;
+        head = mergeSort(head);
+        return head;
+    }
+};
