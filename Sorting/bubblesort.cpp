15cbe366bd95066b05e21bb724f4f5a790e9dbb5
Avishek <kumaravishek84@gmail.com>
2021-06-01 12:42:58 +0530

    bubble sort optimised

--- Sorting/bubblesort.cpp
+++ Sorting/bubblesort.cpp
@@ -4,11 +4,18 @@ void bubbleSort(int *a, int n)
 {
     for (int i = 0; i < n - 1; i++)
     {
+        int swapCnt = 0;
         for (int j = 0; j < n - i - 1; j++)
         {
             if (a[j] > a[j + 1])
+            {
                 swap(a[j], a[j + 1]);
+                swapCnt++;
+            }
         }
+        // Array is sort at this point
+        if (swapCnt == 0)
+            break;
     }
 }
 int main()
@@ -22,3 +29,8 @@ int main()
     for (auto e : a)
         cout << e << " ";
 }
+
+// tc: n*n(worst and best) but using swapCnt variable we can reduce best case tc to o(n).
+// sc-O(1)
+// Stable sorting algo
+// Inplace
