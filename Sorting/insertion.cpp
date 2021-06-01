b473012b1451a771f00a945a9741344ced3bbb5b
Avishek <kumaravishek84@gmail.com>
2021-06-01 12:56:47 +0530

    insertion sort added

---
+++ Sorting/insertion.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+void insertionSort(int *a, int n)
+{
+    for (int i = 1; i < n; i++)
+    {
+        for (int j = i - 1; j >= 0; j--)
+        {
+            if (a[j] > a[j + 1])
+                swap(a[j], a[j + 1]);
+            else
+                break;
+        }
+    }
+}
+int main()
+{
+    int n;
+    cin >> n;
+    int a[n];
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+    insertionSort(a, n);
+    for (auto e : a)
+        cout << e << " ";
+}
