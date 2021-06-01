772e858f7531058c3ee1848d3c5a3a230a28832c
Avishek <kumaravishek84@gmail.com>
2021-06-01 12:31:04 +0530

    bubble sort added

--- Sorting/bubblesort.cpp
+++ Sorting/bubblesort.cpp
@@ -1,13 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
-void bubbleSort(int *a,int n)
+void bubbleSort(int *a, int n)
 {
-    for(int i=0;i<n-1;i++)
+    for (int i = 0; i < n - 1; i++)
     {
-        for(int j=0;j<n-i-1;j++)
+        for (int j = 0; j < n - i - 1; j++)
         {
-            if(a[j]>a[j+1])
-                swap(a[j],a[j+1]);
+            if (a[j] > a[j + 1])
+                swap(a[j], a[j + 1]);
         }
     }
 }
@@ -21,4 +21,4 @@ int main()
     bubbleSort(a, n);
     for (auto e : a)
         cout << e << " ";
-} 
+}
